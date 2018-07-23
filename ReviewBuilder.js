fdc7a2cf076b46e0db018fccdd5d1808c1ade4f2
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2018-07-23 16:34:08 +0900

    Finished buildReviewsPromises method
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Hiroyuki Momoi <hirocorp01@gmail.com>

--- ReviewBuilder.js
+++ ReviewBuilder.js
@@ -68,7 +68,57 @@ class ReviewBuilder {
     });
   }
 
-  buildReviewsPromises() {}
+  buildReviewsPromises() {
+    const productPromise = new Promise((resolve, reject) => {
+      const productsMap = {};
+      let products;
+      fs.readFile("./data/products.json", "utf-8", (error, data) => {
+        if (error) reject(error);
+        products = JSON.parse(data);
+        products.forEach((product) => {
+          productsMap[product.id] = product.name;
+        });
+        resolve(productsMap);
+      });
+    });
+    const userPromise = new Promise((resolve, reject) => {
+      let users;
+      const usersMap = {};
+      fs.readFile("./data/users.json", "utf-8", (error, data) => {
+        if (error) reject(error);
+        users = JSON.parse(data);
+        users.forEach((user) => {
+          usersMap[user.id] = user.username;
+        });
+        resolve(usersMap);
+      });
+    });
+    const reviewsPromise = new Promise((resolve, reject) => {
+      let reviews;
+      fs.readFile("./data/reviews.json", "utf-8", (error, data) => {
+        if (error) reject(error);
+        reviews = JSON.parse(data);
+        resolve(reviews);
+      });
+    });
+
+    const promise = Promise.all([
+      productPromise,
+      userPromise,
+      reviewsPromise,
+    ]).then(([productsMap, usersMap, reviews]) => {
+      return reviews.map((review) => {
+        const productObj = {
+          productName: productsMap[review.productId],
+          username: usersMap[review.userId],
+          text: review.text,
+          rating: review.rating,
+        };
+        return productObj;
+      });
+    });
+    return promise;
+  }
 
   buildReviewsAsyncAwait() {}
 }
